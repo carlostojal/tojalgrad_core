93caf80cb77bc5dd3fe1a658717455d6d138050d
carlostojal <carlos.tojal@hotmail.com>
2023-05-30 10:01:47 +0100

    create layer tests. empty model tests

---
+++ test/LayerTests.cpp
@@ -0,0 +1,47 @@
+//
+// Created by carlostojal on 30-05-2023.
+//
+
+#include <gtest/gtest.h>
+#include <tojalgrad/nn/layers/Layer.h>
+#include <tojalgrad/nn/layers/Linear.h>
+#include <tojalgrad/nn/Activation.h>
+
+using namespace tojalgrad::nn;
+
+class LayerTests : public ::testing::Test {
+
+
+};
+
+TEST_F(LayerTests, linearLayerInstantiation) {
+
+    EXPECT_NO_THROW(layers::Linear linear = layers::Linear(2, 5, Activation::sigmoid));
+}
+
+TEST_F(LayerTests, invalidLayerInstantiation) {
+
+    EXPECT_THROW(layers::Linear linear = layers::Linear(-1, -5, Activation::sigmoid), std::runtime_error);
+}
+
+TEST_F(LayerTests, invalidInputLayerForward) {
+
+    // this layer expects a vector of size 2 as input
+    layers::Linear linear = layers::Linear(2, 5, Activation::sigmoid);
+
+    Eigen::VectorXf vec(3); // size 3: invalid
+    vec << 1.2f, 1.4f, 2.4f;
+
+    EXPECT_THROW(linear.forward(vec), std::runtime_error);
+}
+
+TEST_F(LayerTests, validInputLayerForward) {
+
+    // this layer expects a vector of size 2 as input
+    layers::Linear linear = layers::Linear(2, 5, Activation::sigmoid);
+
+    Eigen::VectorXf vec(2); // size 2
+    vec << 1.2f, 1.4f;
+
+    EXPECT_NO_THROW(linear.forward(vec));
+}
---
+++ test/ModelTests.cpp
@@ -0,0 +1,17 @@
+//
+// Created by carlostojal on 30-05-2023.
+//
+
+#include <gtest/gtest.h>
+#include <tojalgrad/nn/Model.h>
+#include <tojalgrad/nn/layers/Linear.h>
+#include <tojalgrad/nn/Activation.h>
+
+using namespace tojalgrad::nn;
+
+class ModelTests : public ::testing::Test {
+
+
+};
+
+// TODO
